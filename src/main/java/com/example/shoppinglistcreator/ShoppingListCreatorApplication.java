424956cffad6e7021d3debf97ec35426511dfddd
Administrator <k13mono@wp.pl>
2023-10-29 20:21:01 +0100

    init commit

---
+++ src/main/java/com/example/shoppinglistcreator/ShoppingListCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoppinglistcreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingListCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingListCreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shoppinglistcreator/ShoppingListCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoppinglistcreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingListCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
